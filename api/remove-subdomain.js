5242aaa528e23ea6500468b62da4bf797aaedfe1
omnifrog-art <omni.frog.art@gmail.com>
2025-04-26 23:03:48 +0100

    Update remove-subdomain.js

--- api/remove-subdomain.js
+++ api/remove-subdomain.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Only POST requests allowed' });
